6be1fe72a8f874b0fc0481a345860a0c75a5e43f
u631510 <laseba@teco.com.ar>
2022-11-15 13:17:39 -0300

    Desafio Entregable Clase 8

---
+++ desafio_clase_8/server.js
@@ -0,0 +1,83 @@
+const express = require ('express');
+const { Router } = express;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+const productos = [];
+
+const routerProductos = new Router();
+
+routerProductos.get('/', (req, res) => {
+    res.json(productos);
+})
+
+routerProductos.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    const productFound = productos.find((p)=> p.id == id);
+
+    if (productFound) res.json(productFound)
+    else res.json({ error : 'producto no encontrado' });
+})
+
+routerProductos.post('/', (req, res) => {
+
+    let productosLenght = Object.keys(productos).length;
+    if (productosLenght > 0) {
+        let lastProduct = productos[productosLenght-1];
+        let idMax = lastProduct.id;
+        let newId = idMax + 1;
+
+        const newProduct = {...req.body, ...{id: newId}};
+   
+        productos.push(newProduct);  
+        res.json(newProduct);
+    }else{
+        let idMax = 1;
+        const newProduct = {...req.body, ...{id: idMax}};
+
+        productos.push(newProduct);
+        res.json(newProduct);
+    }
+});
+
+routerProductos.put('/:id', (req, res) => {
+    const  id  = req.params.id;
+    const productFound = productos.find((ele) => ele.id == id)
+    if(!productFound) {
+        res.json({error : 'producto no encontrado'});
+    }else{
+        const indexProductFound = productos.indexOf(productFound);
+        productos.splice(indexProductFound, 1, {...req.body, ...{id : id}});
+        res.json("Producto agregado correctamente");
+    }
+})
+
+routerProductos.delete('/:id', (req, res) => {
+
+    const id  = req.params.id
+
+    const productFound = productos.find((p)=> p.id == id);
+
+    if (!productFound) {
+        res.json({ error : 'producto no encontrado' })
+    }else{
+        const indexProductFound = productos.indexOf(productFound);
+        productos.splice(indexProductFound, 1);
+        res.json("Producto eliminado correctamente");
+    }
+})
+
+
+app.use ("/api/productos/", routerProductos)
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log('escuchando en el puerto ' + PORT)
+})
+server.on("error", error => console.log(error))
+
